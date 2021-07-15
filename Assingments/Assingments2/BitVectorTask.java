7b2e0cf74b54720c47433917cffa96dd5c493627
Sunilkumarmalhi2020 <68479996+Sunilkumarmalhi2020@users.noreply.github.com>
2021-07-15 14:11:16 +0500

    Update BitVectorTask.java

--- Assingments/Assingments2/BitVectorTask.java
+++ Assingments/Assingments2/BitVectorTask.java
@@ -5,6 +5,8 @@ public class BitVectorTask {
 	public static boolean checkCharacter(String a,String b){
 
 		for(char c:a.toCharArray()){
+			//(c-'a') let suppose c=z then(z-a) it gives 25 , 1<<25 it shifts bits to left
+			// if we put 'A' instead of 'a' then (z-A) which gives 1<<57 which may shift bits above then 32 bits
 			int charBitVector=1<<(c-'a');
 			for(char d:b.toCharArray()){
 				int charBitVect=1<<(d-'a');
